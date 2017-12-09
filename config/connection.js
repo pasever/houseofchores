90fd0114f19455cf65eede97ce022ad5aa216941
Gerald Porter <porter.glp2010@gmail.com>
2017-12-09 11:58:56 -0500

    updates

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "root",
+    password: "glpMySQL2017",
     database: "chores_db"
 });
 
---
+++ models/chores.js
@@ -0,0 +1,24 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var chores = {
+    all: function(cb) {
+        orm.selectAll("tasks", function(res) {
+            cb(res);
+        });
+    },
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+        orm.create("tasks", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    update: function(objColVals, condition, cb) {
+        orm.update("tasks", objColVals, condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = chores;
