37c5ce1c3b111434d0ab0d2e25d01df94ee72aa7
Pasever <pas1034ever@gmail.com>
2017-12-12 20:19:25 -0500

    a.1.7

--- config/orm.js
+++ config/orm.js
@@ -3,39 +3,80 @@ var connection = require("./connection.js");
 // Object Relational Mapper (ORM)
 
 var orm = {
-    selectAll: function(tableInput) {
-        var queryString = "SELECT tasks.task_id, tasks.taskname, user_chores.user_id, user.username " +
-            "FROM ((tasks " +
-            "INNER JOIN user_chores ON tasks.task_id = user_chores.task_id) " +
-            "INNER JOIN user ON user.user_id = user_chores.user_id); "
-        connection.query(queryString, [tableInput], function(err, result) {
-            console.log(result);
+    selectAll: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
         });
     },
-    //selectAll: function(tableInput) {
-    //    var queryString = "SELECT * FROM ??";
-    //    connection.query(queryString, [tableInput], function(err, result) {
-    //        console.log(result);
-    //    });
-    //},
-    selectWhere: function(tableInput, colToSearch, valOfCol) {
+    selectWhere: function(tableInput, colToSearch, valOfCol, cb) {
         var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+        console.log('qry:', queryString);
         connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
-            console.log(result);
+            if (err) {
+                throw err;
+            }
+            cb(result);
         });
     },
-    selectAndOrder: function(whatToSelect, table, orderCol) {
+    selectAndOrder: function(whatToSelect, table, orderCol, cb) {
         var queryString = "SELECT ?? FROM ?? ORDER BY ?? DESC";
         console.log(queryString);
         connection.query(queryString, [whatToSelect, table, orderCol], function(err, result) {
-            console.log(result);
+            if (err) {
+                throw err;
+            }
+            cb(result);
         });
     },
-    findWhoHasMost: function(tableOneCol, tableTwoForeignKey, tableOne, tableTwo) {
+    findWhoHasMost: function(tableOneCol, tableTwoForeignKey, tableOne, tableTwo, cb) {
         var queryString = "SELECT ??, COUNT(??) AS count FROM ?? LEFT JOIN ?? ON ??.??= ??.id GROUP BY ?? ORDER BY count DESC LIMIT 1";
-
         connection.query(queryString, [tableOneCol, tableOneCol, tableOne, tableTwo, tableTwo, tableTwoForeignKey, tableOne, tableOneCol], function(err, result) {
-            console.log(result);
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    create: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+    // An example of objColVals would be {name: panther, sleepy: true}
+    update: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
         });
     }
 };
--- public/assets/javascript/main.js
+++ public/assets/javascript/main.js
@@ -4,13 +4,10 @@ var data = [
     id: 1,
     src: "http://fancygrid.com/images/staff/boy-4.png",
     name: 'Ted',
-    surname: 'Smith',
-    position: 'Java Developer',
-    email: 'ted.smith@gmail.com',
-    company: 'Electrical Systems',
-    age: 30,
-    education: 'High School Of Cambridge',
-    knownledge: 'Java, Ruby'
+    monday: 'Do dishes',
+    tuesday: ''
+    
+
   }, {
     id: 2,
     src: "http://fancygrid.com/images/staff/boy-1.png",
@@ -64,44 +61,11 @@ $(function() {
     theme: 'dark',
     renderTo: 'container',
     title: 'House of ${HouseOfChores Name}',
-    width: 800,
+    width: 1150,
     height: 565,
-    cellHeight: 90,
+    cellHeight: 100,
     data: data,
     selModel: 'cells',
-    tbar: [
-      {
-        //theme: 'dark',
-        type: 'segbutton',
-        items: [
-          {
-            text: '1 Day',
-            toggleGroup: 'sel',
-            handler: function(b) {
-              grid.setSelModel('columns');
-            }
-          }, {
-            text: '3 days',
-            toggleGroup: 'sel',
-            handler: function(b) {
-              grid.setSelModel('row');
-            }
-          }, {
-            text: '7 Days',
-            toggleGroup: 'sel',
-            handler: function(b) {
-              grid.setSelModel('rows');
-            }
-          }, {
-            text: '30 Days',
-            toggleGroup: 'sel',
-            handler: function(b) {
-              grid.setSelModel('rows');
-            }
-          }
-        ]
-      }
-    ],
     defaults: {
       type: 'string',
       width: 150,
@@ -124,29 +88,50 @@ $(function() {
         type: 'image',
         title: 'Photo',
         index: 'src',
+        width: 120,
         cls: 'photo',
         align: 'center'
       }, {
         index: 'name',
         title: 'Name',
+        width: 100,
         cls: 'id-column-cls',
         align: 'center'
       }, {
-        index: 'age',
-        title: 'Age',
-        type: 'number',
+        index: 'monday',
+        title: 'Monday',
         align: 'center',
         cellAlign: 'center'
-      
       }, {
-        index: 'position',
-        title: 'Position in the House',
-        cls: 'id-column-cls',
-        align: 'center'
+        index: 'tuesday',
+        title: 'Tuesday',
+        align: 'center',
+        cellAlign: 'center'
       }, {
-        index: 'knownledge',
-        title: 'Tasks assigned',
-        align: 'center'
+        index: 'wednesday',
+        title: 'Wednesday',
+        align: 'center',
+        cellAlign: 'center'
+      }, {
+        index: 'thursday',
+        title: 'Thursday',
+        align: 'center',
+        cellAlign: 'center'
+      }, {
+        index: 'friday',
+        title: 'Friday',
+        align: 'center',
+        cellAlign: 'center'
+      }, {
+        index: 'saturday',
+        title: 'Saturday',
+        align: 'center',
+        cellAlign: 'center'
+      }, {
+        index: 'sunday',
+        title: 'Sunday',
+        align: 'center',
+        cellAlign: 'center'
       }
     ]
   });
@@ -154,56 +139,56 @@ $(function() {
 });
 
 
-var ChoresList = document.getElementsByTagName("LI");
-
-for (var i = 0; i < ChoresList.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  ChoresList[i].appendChild(span);
-}
-
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-for (var i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-
-// // Add a "checked" symbol when clicking on a list item
-// var list = document.querySelector('ul');
-// list.addEventListener('click', function(ev) {
-//   if (ev.target.tagName === 'LI') {
-//     ev.target.classList.toggle('checked');
+// var ChoresList = document.getElementsByTagName("LI");
+// 
+// for (var i = 0; i < ChoresList.length; i++) {
+//   var span = document.createElement("SPAN");
+//   var txt = document.createTextNode("\u00D7");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   ChoresList[i].appendChild(span);
+// }
+// 
+// // Click on a close button to hide the current list item
+// var close = document.getElementsByClassName("close");
+// for (var i = 0; i < close.length; i++) {
+//   close[i].onclick = function() {
+//     var div = this.parentElement;
+//     div.style.display = "none";
 //   }
-// }, false);
-
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
-}
+// }
+// 
+// // // Add a "checked" symbol when clicking on a list item
+// // var list = document.querySelector('ul');
+// // list.addEventListener('click', function(ev) {
+// //   if (ev.target.tagName === 'LI') {
+// //     ev.target.classList.toggle('checked');
+// //   }
+// // }, false);
+// 
+// // Create a new list item when clicking on the "Add" button
+// function newElement() {
+//   var li = document.createElement("li");
+//   var inputValue = document.getElementById("myInput").value;
+//   var t = document.createTextNode(inputValue);
+//   li.appendChild(t);
+//   if (inputValue === '') {
+//     alert("You must write something!");
+//   } else {
+//     document.getElementById("myUL").appendChild(li);
+//   }
+//   document.getElementById("myInput").value = "";
+// 
+//   var span = document.createElement("SPAN");
+//   var txt = document.createTextNode("\u00D7");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   li.appendChild(span);
+// 
+//   for (i = 0; i < close.length; i++) {
+//     close[i].onclick = function() {
+//       var div = this.parentElement;
+//       div.style.display = "none";
+//     }
+//   }
+// }
