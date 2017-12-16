b149247a2dfcf7588b2513d9e0eae5e2e85f6585
Pasever <pas1034ever@gmail.com>
2017-12-15 19:58:58 -0500

    a.2.4

--- public/assets/javascript/main.js
+++ public/assets/javascript/main.js
@@ -1,44 +1,46 @@
-$('#add-task').on('click', function () {
+$('#add-task').on('click', function() {
 
   let day = $('#sel1').val();
   let task = $('#sel2').val();
 
   console.log('Day', day);
-  console.log('Task', task);
+  //console.log('Task', task);
 
 });
 
-$.ajax({
-  method: "GET",
-  url: "/api/chores"
-}).done(function (data) {
+$.ajax({method: "GET", url: "/api/chores"}).done(function(data) {
   let choreArray = [];
   let choreObj = data['chores'];
   for (i in choreObj) {
     choreArray.push(choreObj[i]);
   }
 
-  $(function () {
+  $(function() {
 
     var grid = new FancyGrid({
       theme: 'dark',
       renderTo: 'container',
-      title: 'House of ${HouseOfChores Name}',
+      barHeight: 37,
+      titleHeight: 50,
+      cellHeaderHeight: 40,
+      title: 'House of Chores Name',
       width: 1150,
       height: 565,
+      panelBodyBorders: 50,
       cellHeight: 100,
       data: choreArray,
       selModel: 'cells',
       defaults: {
         type: 'string',
-        width: 150,
+        width: 135,
         sortable: true,
         resizable: true,
         ellipsis: true
       },
       columnLines: false,
       tableLines: false,
-      columns: [{
+      columns: [
+        {
           index: 'user_id',
           title: '',
           locked: true,
@@ -54,16 +56,19 @@ $.ajax({
         //   width: 120,
         //   cls: 'photo',
         //   align: 'center'
-        // }, 
+        // },
         {
           index: 'username',
           title: 'Name',
-          width: 100,
+          locked: true,
+          width: 150,
           cls: 'id-column-cls',
           align: 'center'
         }, {
-          index: 'Monday',
+          index: 'Monday',  
           title: 'Monday',
+          type: 'combo',
+          data: [],
           align: 'center',
           cellAlign: 'center'
         }, {
@@ -102,297 +107,295 @@ $.ajax({
   });
   //dataLoaded(data);
 
-// function dataLoaded(data) {
-// 
-//   for (const [key, value] of Object.entries(data)) {
-//     console.log(key, value)
-//   }
-// 
-// }
+  // function dataLoaded(data) {
+  //
+  //   for (const [key, value] of Object.entries(data)) {
+  //     console.log(key, value)
+  //   }
+  //
+  // }
 
+  // var data = [
+  //   {
+  //     id: 1,
+  //     src: "http://fancygrid.com/images/staff/boy-4.png",
+  //     username: 'Ted',
+  //     monday: `$(www.google.com)`,
+  //     tuesday: ''
+  //   }, {
+  //     id: 2,
+  //     src: "http://fancygrid.com/images/staff/boy-1.png",
+  //     name: 'Ed',
+  //     surname: 'Johnson',
+  //     position: 'C/C++ Market Data Developer',
+  //     email: 'ed.johnson@gmail.com',
+  //     company: 'Energy and Oil',
+  //     age: 35,
+  //     education: 'High School Of Cambridge',
+  //     knownledge: 'C++'
+  //   }, {
+  //     id: 3,
+  //     src: "http://fancygrid.com/images/staff/boy-2.png",
+  //     name: 'Sam',
+  //     surname: 'Williams',
+  //     position: 'Technical Analyst',
+  //     email: 'sam.williams@gmail.com',
+  //     company: 'Airbus',
+  //     age: 38,
+  //     education: 'High School Of Cambridge',
+  //     knownledge: 'Javascript'
+  //   }, {
+  //     id: 4,
+  //     src: "http://fancygrid.com/images/staff/boy-3.png",
+  //     name: 'Alexander',
+  //     surname: 'Brown',
+  //     position: 'Project Manager',
+  //     email: 'alexander.brown@gmail.com',
+  //     company: 'Renault',
+  //     age: 24,
+  //     education: 'High School Of Cambridge',
+  //     knownledge: ''
+  //   }, {
+  //     id: 5,
+  //     src: "http://fancygrid.com/images/staff/boy-5.png",
+  //     name: 'Nicholas',
+  //     surname: 'Miller',
+  //     position: 'Senior Software Engineer',
+  //     email: 'nicholas.miller@gmail.com',
+  //     company: 'Adobe',
+  //     age: 33,
+  //     education: 'High School Of Cambridge',
+  //     knownledge: 'Unix, C/C++'
+  //   }
+  // ];
+  //
+  // $(function() {
+  //
+  //   var grid = new FancyGrid({
+  //     theme: 'dark',
+  //     renderTo: 'container',
+  //     title: 'House of ${HouseOfChores Name}',
+  //     width: 1150,
+  //     height: 565,
+  //     cellHeight: 100,
+  //     data: data,
+  //     selModel: 'cells',
+  //     defaults: {
+  //       type: 'string',
+  //       width: 150,
+  //       sortable: true,
+  //       resizable: true,
+  //       ellipsis: true
+  //     },
+  //     columnLines: false,
+  //     tableLines: false,
+  //     columns: [
+  //       {
+  //         index: 'id',
+  //         title: '',
+  //         locked: true,
+  //         width: 50,
+  //         type: 'number',
+  //         align: 'center',
+  //         cls: 'id-column-cls'
+  //       }, {
+  //         type: 'image',
+  //         title: 'Photo',
+  //         index: 'src',
+  //         width: 120,
+  //         cls: 'photo',
+  //         align: 'center'
+  //       }, {
+  //         index: 'username',
+  //         title: 'Name',
+  //         width: 100,
+  //         cls: 'id-column-cls',
+  //         align: 'center'
+  //       }, {
+  //         index: 'monday',
+  //         title: 'Monday',
+  //         align: 'center',
+  //         cellAlign: 'center'
+  //       }, {
+  //         index: 'tuesday',
+  //         title: 'Tuesday',
+  //         align: 'center',
+  //         cellAlign: 'center'
+  //       }, {
+  //         index: 'wednesday',
+  //         title: 'Wednesday',
+  //         align: 'center',
+  //         cellAlign: 'center'
+  //       }, {
+  //         index: 'thursday',
+  //         title: 'Thursday',
+  //         align: 'center',
+  //         cellAlign: 'center'
+  //       }, {
+  //         index: 'friday',
+  //         title: 'Friday',
+  //         align: 'center',
+  //         cellAlign: 'center'
+  //       }, {
+  //         index: 'saturday',
+  //         title: 'Saturday',
+  //         align: 'center',
+  //         cellAlign: 'center'
+  //       }, {
+  //         index: 'sunday',
+  //         title: 'Sunday',
+  //         align: 'center',
+  //         cellAlign: 'center'
+  //       }
+  //     ]
+  //   });
+  // });
 
-
-// var data = [
-//   {
-//     id: 1,
-//     src: "http://fancygrid.com/images/staff/boy-4.png",
-//     username: 'Ted',
-//     monday: `$(www.google.com)`,
-//     tuesday: ''
-//   }, {
-//     id: 2,
-//     src: "http://fancygrid.com/images/staff/boy-1.png",
-//     name: 'Ed',
-//     surname: 'Johnson',
-//     position: 'C/C++ Market Data Developer',
-//     email: 'ed.johnson@gmail.com',
-//     company: 'Energy and Oil',
-//     age: 35,
-//     education: 'High School Of Cambridge',
-//     knownledge: 'C++'
-//   }, {
-//     id: 3,
-//     src: "http://fancygrid.com/images/staff/boy-2.png",
-//     name: 'Sam',
-//     surname: 'Williams',
-//     position: 'Technical Analyst',
-//     email: 'sam.williams@gmail.com',
-//     company: 'Airbus',
-//     age: 38,
-//     education: 'High School Of Cambridge',
-//     knownledge: 'Javascript'
-//   }, {
-//     id: 4,
-//     src: "http://fancygrid.com/images/staff/boy-3.png",
-//     name: 'Alexander',
-//     surname: 'Brown',
-//     position: 'Project Manager',
-//     email: 'alexander.brown@gmail.com',
-//     company: 'Renault',
-//     age: 24,
-//     education: 'High School Of Cambridge',
-//     knownledge: ''
-//   }, {
-//     id: 5,
-//     src: "http://fancygrid.com/images/staff/boy-5.png",
-//     name: 'Nicholas',
-//     surname: 'Miller',
-//     position: 'Senior Software Engineer',
-//     email: 'nicholas.miller@gmail.com',
-//     company: 'Adobe',
-//     age: 33,
-//     education: 'High School Of Cambridge',
-//     knownledge: 'Unix, C/C++'
-//   }
-// ];
-// 
-// $(function() {
-// 
-//   var grid = new FancyGrid({
-//     theme: 'dark',
-//     renderTo: 'container',
-//     title: 'House of ${HouseOfChores Name}',
-//     width: 1150,
-//     height: 565,
-//     cellHeight: 100,
-//     data: data,
-//     selModel: 'cells',
-//     defaults: {
-//       type: 'string',
-//       width: 150,
-//       sortable: true,
-//       resizable: true,
-//       ellipsis: true
-//     },
-//     columnLines: false,
-//     tableLines: false,
-//     columns: [
-//       {
-//         index: 'id',
-//         title: '',
-//         locked: true,
-//         width: 50,
-//         type: 'number',
-//         align: 'center',
-//         cls: 'id-column-cls'
-//       }, {
-//         type: 'image',
-//         title: 'Photo',
-//         index: 'src',
-//         width: 120,
-//         cls: 'photo',
-//         align: 'center'
-//       }, {
-//         index: 'username',
-//         title: 'Name',
-//         width: 100,
-//         cls: 'id-column-cls',
-//         align: 'center'
-//       }, {
-//         index: 'monday',
-//         title: 'Monday',
-//         align: 'center',
-//         cellAlign: 'center'
-//       }, {
-//         index: 'tuesday',
-//         title: 'Tuesday',
-//         align: 'center',
-//         cellAlign: 'center'
-//       }, {
-//         index: 'wednesday',
-//         title: 'Wednesday',
-//         align: 'center',
-//         cellAlign: 'center'
-//       }, {
-//         index: 'thursday',
-//         title: 'Thursday',
-//         align: 'center',
-//         cellAlign: 'center'
-//       }, {
-//         index: 'friday',
-//         title: 'Friday',
-//         align: 'center',
-//         cellAlign: 'center'
-//       }, {
-//         index: 'saturday',
-//         title: 'Saturday',
-//         align: 'center',
-//         cellAlign: 'center'
-//       }, {
-//         index: 'sunday',
-//         title: 'Sunday',
-//         align: 'center',
-//         cellAlign: 'center'
-//       }
-//     ]
-//   });
-// });
-
-//
-//
-// $('#add-task').on('click', function() {
-//
-//   let day = $('#sel1').val();
-//   let task = $('#sel2').val();
-//
-//   console.log('Day', day);
-//   console.log('Task', task);
-//
-// });
-//
-// var data = [
-//   {
-//     id: 1,
-//     src: "http://fancygrid.com/images/staff/boy-4.png",
-//     name: 'Ted',
-//     monday: `$(www.google.com)`,
-//     tuesday: ''
-//   }, {
-//     id: 2,
-//     src: "http://fancygrid.com/images/staff/boy-1.png",
-//     name: 'Ed',
-//     surname: 'Johnson',
-//     position: 'C/C++ Market Data Developer',
-//     email: 'ed.johnson@gmail.com',
-//     company: 'Energy and Oil',
-//     age: 35,
-//     education: 'High School Of Cambridge',
-//     knownledge: 'C++'
-//   }, {
-//     id: 3,
-//     src: "http://fancygrid.com/images/staff/boy-2.png",
-//     name: 'Sam',
-//     surname: 'Williams',
-//     position: 'Technical Analyst',
-//     email: 'sam.williams@gmail.com',
-//     company: 'Airbus',
-//     age: 38,
-//     education: 'High School Of Cambridge',
-//     knownledge: 'Javascript'
-//   }, {
-//     id: 4,
-//     src: "http://fancygrid.com/images/staff/boy-3.png",
-//     name: 'Alexander',
-//     surname: 'Brown',
-//     position: 'Project Manager',
-//     email: 'alexander.brown@gmail.com',
-//     company: 'Renault',
-//     age: 24,
-//     education: 'High School Of Cambridge',
-//     knownledge: ''
-//   }, {
-//     id: 5,
-//     src: "http://fancygrid.com/images/staff/boy-5.png",
-//     name: 'Nicholas',
-//     surname: 'Miller',
-//     position: 'Senior Software Engineer',
-//     email: 'nicholas.miller@gmail.com',
-//     company: 'Adobe',
-//     age: 33,
-//     education: 'High School Of Cambridge',
-//     knownledge: 'Unix, C/C++'
-//   }
-// ];
-//
-// $(function() {
-//
-//   var grid = new FancyGrid({
-//     theme: 'dark',
-//     renderTo: 'container',
-//     title: 'House of ${HouseOfChores Name}',
-//     width: 1150,
-//     height: 565,
-//     cellHeight: 100,
-//     data: data,
-//     selModel: 'cells',
-//     defaults: {
-//       type: 'string',
-//       width: 150,
-//       sortable: true,
-//       resizable: true,
-//       ellipsis: true
-//     },
-//     columnLines: false,
-//     tableLines: false,
-//     columns: [
-//       {
-//         index: 'id',
-//         title: '',
-//         locked: true,
-//         width: 50,
-//         type: 'number',
-//         align: 'center',
-//         cls: 'id-column-cls'
-//       }, {
-//         type: 'image',
-//         title: 'Photo',
-//         index: 'src',
-//         width: 120,
-//         cls: 'photo',
-//         align: 'center'
-//       }, {
-//         index: 'name',
-//         title: 'Name',
-//         width: 100,
-//         cls: 'id-column-cls',
-//         align: 'center'
-//       }, {
-//         index: 'monday',
-//         title: 'Monday',
-//         align: 'center',
-//         cellAlign: 'center'
-//       }, {
-//         index: 'tuesday',
-//         title: 'Tuesday',
-//         align: 'center',
-//         cellAlign: 'center'
-//       }, {
-//         index: 'wednesday',
-//         title: 'Wednesday',
-//         align: 'center',
-//         cellAlign: 'center'
-//       }, {
-//         index: 'thursday',
-//         title: 'Thursday',
-//         align: 'center',
-//         cellAlign: 'center'
-//       }, {
-//         index: 'friday',
-//         title: 'Friday',
-//         align: 'center',
-//         cellAlign: 'center'
-//       }, {
-//         index: 'saturday',
-//         title: 'Saturday',
-//         align: 'center',
-//         cellAlign: 'center'
-//       }, {
-//         index: 'sunday',
-//         title: 'Sunday',
-//         align: 'center',
-//         cellAlign: 'center'
-//       }
-//     ]
-//   });
-// });
+  //
+  //
+  // $('#add-task').on('click', function() {
+  //
+  //   let day = $('#sel1').val();
+  //   let task = $('#sel2').val();
+  //
+  //   console.log('Day', day);
+  //   console.log('Task', task);
+  //
+  // });
+  //
+  // var data = [
+  //   {
+  //     id: 1,
+  //     src: "http://fancygrid.com/images/staff/boy-4.png",
+  //     name: 'Ted',
+  //     monday: `$(www.google.com)`,
+  //     tuesday: ''
+  //   }, {
+  //     id: 2,
+  //     src: "http://fancygrid.com/images/staff/boy-1.png",
+  //     name: 'Ed',
+  //     surname: 'Johnson',
+  //     position: 'C/C++ Market Data Developer',
+  //     email: 'ed.johnson@gmail.com',
+  //     company: 'Energy and Oil',
+  //     age: 35,
+  //     education: 'High School Of Cambridge',
+  //     knownledge: 'C++'
+  //   }, {
+  //     id: 3,
+  //     src: "http://fancygrid.com/images/staff/boy-2.png",
+  //     name: 'Sam',
+  //     surname: 'Williams',
+  //     position: 'Technical Analyst',
+  //     email: 'sam.williams@gmail.com',
+  //     company: 'Airbus',
+  //     age: 38,
+  //     education: 'High School Of Cambridge',
+  //     knownledge: 'Javascript'
+  //   }, {
+  //     id: 4,
+  //     src: "http://fancygrid.com/images/staff/boy-3.png",
+  //     name: 'Alexander',
+  //     surname: 'Brown',
+  //     position: 'Project Manager',
+  //     email: 'alexander.brown@gmail.com',
+  //     company: 'Renault',
+  //     age: 24,
+  //     education: 'High School Of Cambridge',
+  //     knownledge: ''
+  //   }, {
+  //     id: 5,
+  //     src: "http://fancygrid.com/images/staff/boy-5.png",
+  //     name: 'Nicholas',
+  //     surname: 'Miller',
+  //     position: 'Senior Software Engineer',
+  //     email: 'nicholas.miller@gmail.com',
+  //     company: 'Adobe',
+  //     age: 33,
+  //     education: 'High School Of Cambridge',
+  //     knownledge: 'Unix, C/C++'
+  //   }
+  // ];
+  //
+  // $(function() {
+  //
+  //   var grid = new FancyGrid({
+  //     theme: 'dark',
+  //     renderTo: 'container',
+  //     title: 'House of ${HouseOfChores Name}',
+  //     width: 1150,
+  //     height: 565,
+  //     cellHeight: 100,
+  //     data: data,
+  //     selModel: 'cells',
+  //     defaults: {
+  //       type: 'string',
+  //       width: 150,
+  //       sortable: true,
+  //       resizable: true,
+  //       ellipsis: true
+  //     },
+  //     columnLines: false,
+  //     tableLines: false,
+  //     columns: [
+  //       {
+  //         index: 'id',
+  //         title: '',
+  //         locked: true,
+  //         width: 50,
+  //         type: 'number',
+  //         align: 'center',
+  //         cls: 'id-column-cls'
+  //       }, {
+  //         type: 'image',
+  //         title: 'Photo',
+  //         index: 'src',
+  //         width: 120,
+  //         cls: 'photo',
+  //         align: 'center'
+  //       }, {
+  //         index: 'name',
+  //         title: 'Name',
+  //         width: 100,
+  //         cls: 'id-column-cls',
+  //         align: 'center'
+  //       }, {
+  //         index: 'monday',
+  //         title: 'Monday',
+  //         align: 'center',
+  //         cellAlign: 'center'
+  //       }, {
+  //         index: 'tuesday',
+  //         title: 'Tuesday',
+  //         align: 'center',
+  //         cellAlign: 'center'
+  //       }, {
+  //         index: 'wednesday',
+  //         title: 'Wednesday',
+  //         align: 'center',
+  //         cellAlign: 'center'
+  //       }, {
+  //         index: 'thursday',
+  //         title: 'Thursday',
+  //         align: 'center',
+  //         cellAlign: 'center'
+  //       }, {
+  //         index: 'friday',
+  //         title: 'Friday',
+  //         align: 'center',
+  //         cellAlign: 'center'
+  //       }, {
+  //         index: 'saturday',
+  //         title: 'Saturday',
+  //         align: 'center',
+  //         cellAlign: 'center'
+  //       }, {
+  //         index: 'sunday',
+  //         title: 'Sunday',
+  //         align: 'center',
+  //         cellAlign: 'center'
+  //       }
+  //     ]
+  //   });
+  // });
 })
