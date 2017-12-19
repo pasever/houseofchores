f581ce0da9328c8445fc38291eb6486c0b66adc0
Pasever <pas1034ever@gmail.com>
2017-12-19 17:27:18 -0500

    a.3.1

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
+let connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "chores_db",
-    port: 3306
-});
-
+if(process.env.JAWSDB_URL) {
+    //Heroku deployment
+      connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    //local host
+      connection = mysql.createConnection({
+          root: 3000,
+          host: "localhost",
+          user: "root",
+          password: "",
+          database: "db_name",
+      });
+  };
 connection.connect(function(err) {
     if (err) {
         console.error("error connecting: " + err.stack);
